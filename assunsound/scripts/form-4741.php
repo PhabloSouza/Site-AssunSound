6475e68773463a64ba848ab174b9ac8b94543104
Phablo Souza <phablogm@gmail.com>
2022-02-04 19:38:24 -0300

    commit full site
    site assunsound

---
+++ assunsound/scripts/form-4741.php
@@ -0,0 +1,66 @@
+<?php
+
+require_once('FormProcessor.php');
+
+$form = array(
+    'subject' => 'New Form Submission',
+    'email_message' => 'You have a new form submission',
+    'success_redirect' => '',
+    'sendIpAddress' => true,
+    'email' => array(
+    'from' => '',
+    'to' => ''
+    ),
+    'fields' => array(
+    'name' => array(
+    'order' => 1,
+    'type' => 'string',
+    'label' => 'Name',
+    'required' => true,
+    'errors' => array(
+    'required' => 'Field \'Name\' is required.'
+    )
+    ),
+    'phone' => array(
+    'order' => 2,
+    'type' => 'tel',
+    'label' => 'Telefone',
+    'required' => true,
+    'errors' => array(
+    'required' => 'Field \'Telefone\' is required.'
+    )
+    ),
+    'email' => array(
+    'order' => 3,
+    'type' => 'email',
+    'label' => 'Email',
+    'required' => true,
+    'errors' => array(
+    'required' => 'Field \'Email\' is required.'
+    )
+    ),
+    'address' => array(
+    'order' => 4,
+    'type' => 'string',
+    'label' => 'Endereço',
+    'required' => true,
+    'errors' => array(
+    'required' => 'Field \'Endereço\' is required.'
+    )
+    ),
+    'message' => array(
+    'order' => 5,
+    'type' => 'string',
+    'label' => 'message',
+    'required' => true,
+    'errors' => array(
+    'required' => 'Field \'message\' is required.'
+    )
+    ),
+    )
+    );
+
+    $processor = new FormProcessor();
+    $processor->process($form);
+
+    ?>
